575947c579846c6000c56e07f90cf30d9fb346d8
dipesh@swabhavtechlabs.com <Dipesh-Swabhav>
2022-05-04 13:23:49 +0530

    basic consoleapp

---
+++ RectangleApp/Model/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleApp.Model
+{
+    public class Rectangle
+    {
+        public static int width { get; set; } =0;
+
+        public static int length { get; set; } = 0;
+
+        public static int Calculate_Area(int width, int length)
+        {
+            return width * length;
+        }
+    }
+}
---
+++ RectangleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using RectangleApp.Model;
+using System;
+
+namespace RectangleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle.length = 40;
+            Rectangle.width = 30;
+
+            Console.WriteLine(Rectangle.Calculate_Area(Rectangle.width,Rectangle.length).ToString());
+
+            Rectangle.length = 60;
+            Rectangle.width = 90;
+
+            Console.WriteLine(Rectangle.Calculate_Area(Rectangle.width, Rectangle.length).ToString());
+            Console.ReadKey();
+        }
+    }
+}
